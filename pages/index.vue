12ce628c851daa488800cb46900171fe3b484934
farrelabennaya <abennayafarrel@gmail.com>
2025-10-26 19:19:33 +0700

    Deploy

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const r = useRouter()
+// SSR-safe: return navigateTo agar dieksekusi di server
+await navigateTo('/login', { replace: true })
+</script>
+
+<template></template>
